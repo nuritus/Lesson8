fa641e1601263fab032edf2d156f8292541c5dbe
nuritus <nurit@g.jct.ac.il>
2020-11-30 16:21:57 +0200

    Add project files.

---
+++ APIDL/IDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+namespace APIDL
+
+{
+    public interface IDal
+    {
+        double GetTemparture(int day);
+        WindDirection GetWindDirection(int day);
+
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BlApi;
+using BO;
+using BL;
+
+
+namespace PlConsole
+{
+    class Program
+    {
+        static IBL bl;
+
+        static void Main(string[] args)
+        {
+            // bl = BlFactory.GetBl();
+            bl = new BlImp1();
+            Console.Write("Please enter how many days back: ");
+            int days = int.Parse(Console.ReadLine());
+            for (int d = days; d >= 0; --d)
+            {
+                Weather w = bl.GetWeather(d);
+                Console.WriteLine($"{d} days before - Feeling was: {w.Feeling} Celsius degrees");
+            }
+
+        }
+    }
+}
